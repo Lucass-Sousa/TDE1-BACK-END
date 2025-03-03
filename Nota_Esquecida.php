0cbb8ca6da7f55ca8c46b28195da53d6606a58b7
Lucas Sousa <lucasdevfront7@gmail.com>
2025-03-03 13:26:23 -0300

    Adição da questão resolvida dos teleféricos

--- Nota_Esquecida.php
+++ Nota_Esquecida.php
@@ -1,7 +1,7 @@
 <?php
-    echo "Digite a nota lembrada: \n";
+    echo "Digite a nota lembrada: ";
     $a = intval(fgets(STDIN));
-    echo "Digite a média lembrada: \n";
+    echo "Digite a média lembrada: ";
     $m = intval(fgets(STDIN));
     $b = (2*$m ) - $a;
     echo "A nota esquecida é $b";
